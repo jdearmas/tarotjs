c06010f781f3e846fda7566d730bb3f7956aa75a
John De Armas <dearmas.john1996@gmail.com>
2020-12-10 15:57:41 -0700

    create react image component

--- src/script.js
+++ src/script.js
@@ -1,7 +1,30 @@
+ReactDOM.render(
+    "<h1>Hello, world!</h1>",
+    document.getElementById('root')
+);
 
-function add_tarot_image() {
-    var img = document.createElement("img");
-    img.src  = 'imgs/tarot-10of78-coin1.png'
-    document.body.appendChild(img);
+// function add_tarot_image() {
+//     var img = document.createElement("img");
+//     img.src  = 'imgs/tarot-10of78-coin1.png'
+//     document.body.appendChild(img);
+// }
+
+
+'use strict';
+
+const e = React.createElement;
+
+
+class DisplayAnImage extends React.Component{
+  render() {
+    return e(
+        'img',
+        { src: 'imgs/tarot-10of78-coin1.png' }
+    );
+  }
 }
 
+ReactDOM.render(
+  React.createElement(DisplayAnImage, null, null),
+  document.querySelector("#timg")
+);
