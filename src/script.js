c183519194dfbf05dc6ebff19e0e7d71ac4ec07f
John De Armas <dearmas.john1996@gmail.com>
2020-12-10 16:05:06 -0700

    create react button component to get reading

--- src/script.js
+++ src/script.js
@@ -1,20 +1,37 @@
-ReactDOM.render(
-    "<h1>Hello, world!</h1>",
-    document.getElementById('root')
-);
-
-// function add_tarot_image() {
-//     var img = document.createElement("img");
-//     img.src  = 'imgs/tarot-10of78-coin1.png'
-//     document.body.appendChild(img);
-// }
+// script.js: dynamic logic for tarotjs
 
 
+// React 
 'use strict';
-
 const e = React.createElement;
 
+// React Button Component
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
 
+  render() {
+    if (this.state.clicked) {
+      return 'congrats, you about to make that $$$$$$$$$';
+    }
+    return e(
+      'button',
+      { onClick: () => this.setState({ clicked: true }) },
+    );
+  }
+}
+ReactDOM.render(
+  React.createElement(Button, null, null),
+  document.querySelector("#reading_button")
+);
+
+
+
+
+// React Image Component
+'use strict';
 class DisplayAnImage extends React.Component{
   render() {
     return e(
